27eb25457185a16a06049bb9914a06fecf177bd4
bransfieldjack <bransfieldjack@gmail.com>
2020-05-19 17:18:24 +1000

    Adding smtp functionality to transfer script.

--- sys/transfer_expression_files.py
+++ sys/transfer_expression_files.py
@@ -5,13 +5,33 @@ then writes that to a new location.
 """
 
 import os, tarfile, pandas
+import smtplib
 import glob
 import re
 
 def main():
 
-        
-    reT = re.compile(r'.*?count*?.*?.txt*?')
+    
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'admin@stemformatics.org'
+    smtp_password = '######'
+    body = "Expression transfer script has finished running. "
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+    %s
+    """ % ("admin@stemformatics.org", "bransfieldjack@gmail.com", "Transfer script", body)
+    s = smtplib.SMTP(smtp_server, smtp_port)
+    s.ehlo()
+    s.starttls()
+    s.ehlo
+    s.login(smtp_username, smtp_password)
+
+    # reT = re.compile(r'.*?count*?.*?.txt*?')
+    reT = re.compile(r'count(.+?).txt')
+    
 
     for tar_filename in glob.glob(r'data/*.tar.gz'):
         try:
@@ -19,9 +39,13 @@ def main():
         except IOError as e:
             print (e)
         else:
-            # print([m for m in t.getmembers() if reT.search(m.name)])
-            t.extractall('out', members=[m for m in t.getmembers() if reT.search(m.name)])
+            print([m for m in t.getmembers() if reT.search(m.name)])
+            s.sendmail("admin@stemformatics.org", "bransfieldjack@gmail.com", email_text)
+            s.quit()
+
+            #t.extractall('out', members=[m for m in t.getmembers() if reT.search(m.name)])
 
 if __name__ == "__main__":
     main()
+
 
